25a7582cad04f681ab6ebaef70e6f5f59639c69b
Ramsey Nasser <ram@nas.sr>
2019-08-12 17:21:28 -0400

    Add rollup to build for browsers

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import typescript from 'rollup-plugin-typescript';
+import sourceMaps from 'rollup-plugin-sourcemaps'
+
+export default {
+    input: './coroutines.ts',
+    output: {
+        name: "coroutines",
+        file: "./browser.js",
+        format: "iife",
+        sourcemap: true
+    },
+    plugins: [
+      typescript(),
+      sourceMaps(),
+    ],
+  };
